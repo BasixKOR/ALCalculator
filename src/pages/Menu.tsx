6c044c427bae8e48efc0dfdef5edf815f14770ff
Basix <basix@basix.tech>
2019-06-09 22:54:04 +0900

    Add Menu

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,11 +1,22 @@
 import React, { useContext } from 'react'
 import { AzurLaneContext } from '../lib/azurlane';
 import AkashiLayout from '../AkashiLayout';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const MenuItem = styled(Link)`
+  width: 100%;
+  background-color: dodgerblue;
+  padding: .3em;
+  font-size: 1.3em;
+  color: white;
+  text-decoration: none;
+`
 
 const Menu: React.FC = props => {
   let azurlane = useContext(AzurLaneContext)
   return <AkashiLayout>
-    
+    <MenuItem to="/calc/training">Training EXP</MenuItem>
   </AkashiLayout>
 }
 
